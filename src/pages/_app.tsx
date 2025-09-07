3cdfde70fc8ecf016c98386ee359ce2617c7244e
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-09-08 01:21:24 +0500

    Updated NavBar logic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,26 @@
-import { ClerkProvider } from '@clerk/nextjs';
-import type { AppProps } from 'next/app';
-import '../app/globals.css';
-import { Navbar } from '@/components/NavBar';
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+import "../app/globals.css";
+import { Navbar } from "@/components/NavBar";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  // 👇 Add the route(s) where you DON'T want the navbar
+  const noNavbarRoutes = ["/dashboard"]; // e.g. hide navbar on /signin
+
+  const hideNavbar = noNavbarRoutes.includes(router.pathname);
+
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} {...pageProps}>
-      <Navbar />
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      {...pageProps}
+    >
+      {!hideNavbar && <Navbar />} {/* Navbar hidden only on /signin */}
       <Component {...pageProps} />
     </ClerkProvider>
   );
 }
 
 export default MyApp;
-
-
