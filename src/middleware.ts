0cee8994bb2a90119b661c010af23794d3398c05
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-10-14 13:52:10 +0500

    fix: middleware.ts file for Google Indexing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,26 @@
 // middleware.ts
-import { clerkMiddleware, getAuth } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 import { PLAN_TYPE } from './config/plans';
 
-const publicRoutes = ['/', '/(api)(.*)', '/sign-in', '/sign-up', '/not-allowed']; // ADD ALL PUBLIC ROUTES HERE
+const isPublicRoute = createRouteMatcher([
+    '/', 
+    '/signin(.*)', 
+    '/signup(.*)', 
+    '/not-allowed',
+]);
+
 
 export default clerkMiddleware(async (auth, request) => {
-    
-    // Check if the current route is one of the defined public routes.
-    const isPublicRoute = publicRoutes.some(route => 
-        request.nextUrl.pathname === route || 
-        (route.endsWith('(.*)') && request.nextUrl.pathname.match(new RegExp(`^${route.replace('(.*)', '')}`)))
-    );
 
-    if (isPublicRoute) {
+    if (isPublicRoute(request)) {
         return NextResponse.next();
     }
-
+    
     const ua = request.headers.get('user-agent')?.toLowerCase() || '';
     const isMobile = /iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(ua);
-    const userPlan = PLAN_TYPE // THIS LINE NEEDS TO BE DYNAMICALLY FETCHED
+    
+    const userPlan = PLAN_TYPE 
 
     if (
         userPlan === 'Business' &&
@@ -40,6 +41,7 @@ export default clerkMiddleware(async (auth, request) => {
     return NextResponse.next();
 });
 
+
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+    matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 };
