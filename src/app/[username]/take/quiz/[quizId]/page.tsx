8aca5d287ece3dde1ea9017a90e55299ed2c51d8
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-10-26 01:16:55 +0500

    fix:issue with link

--- src/app/[username]/take/quiz/[quizId]/page.tsx
+++ src/app/[username]/take/quiz/[quizId]/page.tsx
@@ -688,7 +688,7 @@ export default function QuizPage({ params }: QuizPageProps) {
 
       const currentPath = window.location.href;
       
-      const response = await fetch(`https://quizzviz-publish-quiz.up.railway.app/publish/public/quiz/${encodeURIComponent(currentPath)}?key=${encodeURIComponent(formData.quizKey)}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_PUBLISH_QUIZZ_SERVICE_URL}/publish/public/quiz/${encodeURIComponent(currentPath)}?key=${encodeURIComponent(formData.quizKey)}`, {
         method: 'GET',
         headers: {
           'accept': 'application/json',
