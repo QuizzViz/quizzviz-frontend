f68290b3fa5e680654c12205f63d8291fbd7deb1
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-10-26 19:30:22 +0500

    fix: dashboard issues

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 
