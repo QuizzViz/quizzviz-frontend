cb5525c866ac7306976d37cceda0a3e35eb2f12f
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-11-10 01:03:43 +0500

    fix: generate button disabling issue resolve

--- src/components/CreateQuizCard/index.tsx
+++ src/components/CreateQuizCard/index.tsx
@@ -87,8 +87,7 @@ export default function CreateQuizCard({ maxQuestions: propMaxQuestions }: Creat
     } as const;
     
     const maxQuizzes = limits[plan as keyof typeof limits] || 0;
-    // const currentMonthQuizzes = quizUsage.data?.current_month?.quiz_count || 0;
-    const currentMonthQuizzes = 10;
+    const currentMonthQuizzes = quizUsage.data?.current_month?.quiz_count || 0;
     return currentMonthQuizzes >= maxQuizzes;
   }, [planName, quizUsage]);
 
